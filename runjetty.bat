4728f14edfb28620542e95e7222a7475288914e6
Flavio Donzé <flavio.donze@scodi.ch>
2020-04-01 11:54:28 +0200

    setup to provide a OIDC service

---
+++ runjetty.bat
@@ -0,0 +1,4 @@
+copy D:\temp\cas-overlay\build\libs\cas.war D:\Users\fdo\Desktop\jetty-distribution-9.4.27.v20200227\webapps /Y
+
+cd D:\Users\fdo\Desktop\jetty-distribution-9.4.27.v20200227
+"D:\dev\software\jdk13.0.2\bin\java" -jar start.jar > log.txt
